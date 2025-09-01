8629b83af9a64515e62fe9107949b2f329b13651
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-01 14:07:09 +0530

    Time: 5 ms (57.51%), Space: 44.9 MB (27.17%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0) even.add(nums[i]);
+            else odd.add(nums[i]);
+        }
+        Collections.sort(even);
+        Collections.sort(odd);
+        Collections.reverse(odd);
+        int a = 0;
+        int b = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                nums[i]=even.get(a);
+                a++;
+            }
+            else{
+                nums[i]=odd.get(b);
+                b++;
+            }
+        }
+        return nums;
+    }
+}
