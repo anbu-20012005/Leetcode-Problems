23c7137a45914ce5e08c5c75732ea71080663410
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-03 13:43:33 +0530

    Time: 3 ms (55.71%), Space: 41.8 MB (60.86%) - LeetHub

---
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reorderSpaces(String text) {
+        String[] words = text.trim().split("\\s+");
+        int wc = words.length;
+        int sp = 0;
+        for(char ch : text.toCharArray()){
+            if(ch==' ') sp++;
+        }
+        if (words.length == 1) {
+        StringBuilder sb = new StringBuilder(words[0]);
+        for (int i = 0; i < sp; i++) sb.append(" ");
+        return sb.toString();
+        }
+        StringBuilder sb = new StringBuilder();
+        int diff = sp/(wc-1);
+        int rem = sp%(wc-1);
+        String s = "";
+        for(int i=0;i<diff;i++){
+            s = s+" ";
+        }
+        for(int i=0;i<wc;i++){
+            sb.append(words[i]);
+            if(i<wc-1)
+            sb.append(s);
+        }
+        for(int i=0;i<rem;i++) sb.append(" ");
+        return sb.toString();
+    }
+}
