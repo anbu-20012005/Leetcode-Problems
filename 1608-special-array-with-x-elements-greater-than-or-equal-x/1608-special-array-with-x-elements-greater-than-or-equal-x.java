9511350f08bc03ed2a1cc7fd96468d6fcc827b26
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-03 13:55:18 +0530

    Time: 1 ms (80.53%), Space: 41.8 MB (11.03%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        for(int i=0;i<=nums.length;i++){
+            int count = 0;
+            for(int j=0;j<nums.length;j++){
+                if(i<=nums[j]) count++;
+            }
+            if(count==i) return i;
+        }
+        return -1;
+    }
+}
