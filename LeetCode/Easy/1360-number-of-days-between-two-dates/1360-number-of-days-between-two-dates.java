8182acbfd9743097cf0dea70ac01f510314abb6c
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-06 15:27:51 +0530

    Time: 7 ms (41.4%), Space: 42 MB (21.82%) - LeetHub

---
+++ LeetCode/Easy/1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.java
@@ -0,0 +1,9 @@
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+class Solution {
+    public int daysBetweenDates(String date1, String date2) {
+        LocalDate a = LocalDate.parse(date1);
+        LocalDate b = LocalDate.parse(date2);
+        return (int) Math.abs(ChronoUnit.DAYS.between(a,b));
+    }
+}
