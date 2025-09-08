b3b7bc2e603c35637510d221284317fddbbc943b
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-08 22:47:44 +0530

    Time: 0 ms (100%), Space: 41.8 MB (43.02%) - LeetHub

---
+++ LeetCode/Easy/2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,22 @@
+class Solution {
+    static {
+        for(int i = 0; i < 500; i++)
+            countAsterisks("");
+    }
+    public static int countAsterisks(String s) {
+        int count = 0;
+        boolean pair = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '|') {
+                pair = !pair;
+            }
+
+            if (pair == false && ch == '*') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
