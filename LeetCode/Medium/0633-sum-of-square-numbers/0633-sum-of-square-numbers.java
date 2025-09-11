e63828a4ba8ce7461a559e2aaf5c3a697765b2fb
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-11 10:47:30 +0530

    Time: 13 ms (6.11%), Space: 44.2 MB (6.86%) - LeetHub

--- LeetCode/Medium/0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ LeetCode/Medium/0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        Long root = (long)Math.sqrt(c);
+        Long j = (long)Math.sqrt(c);
         int i = 0;
-        Long j = root;
         while(i<=j){
             Long sum = (i*i)+(j*j);
             if(sum==c) return true;
