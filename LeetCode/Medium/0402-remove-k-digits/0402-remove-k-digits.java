5ba183de0c1178d985190ae7868bda750ad48469
anbu-20012005 <anbarasan20012005@gmail.com>
2025-09-28 11:25:42 +0530

    Time: 7 ms (95.88%), Space: 44.5 MB (96.48%) - LeetHub

---
+++ LeetCode/Medium/0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if(k==num.length()) return "0";
+        StringBuilder s = new StringBuilder();
+        for(int i=0;i<num.length();i++){
+            char ch = num.charAt(i);
+            while(s.length()!=0 && k!=0 && s.charAt(s.length()-1)>ch){
+                s.setLength(s.length()-1);
+                k--;
+            }
+            s.append(ch);
+        }
+        if(k>0) s.setLength(s.length()-k);
+        int i = 0;
+        while(i<s.length() && s.charAt(i)=='0') i++;
+        if(i==s.length()) return "0";
+        return s.substring(i).toString();
+    }
+}
